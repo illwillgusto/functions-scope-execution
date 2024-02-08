0560a5f934001896ebc59a8942232bfccfad4d4e
Will Walton <will@endless3dge.com>
2024-02-08 17:28:40 -0600

    Arrow functions
    Understanding the basics of arrow functions and how to format them

---
+++ 07-arrow-functions/script.js
@@ -0,0 +1,34 @@
+// function add(a,b) {
+//     return a + b;
+// }
+
+// Arrow Function, '=>' is called a fat arrow, regular arrow function syntax
+const add = (a, b) => {
+    return a + b;
+}
+
+// Implicit Return
+const subtr = (a, b) => a - b;
+
+// Can leave off () with a single param
+const double = a => a * 2;
+
+// Returning an object
+const createObj = () => ({
+    name: 'Will'
+})
+
+const numbers = [1,2,3,4,5];
+
+numbers.forEach(function (n) {
+    console.log(n);
+})
+
+// Arrow function in a callback, a callback is passing a function into another function like seen below 
+numbers.forEach((n) => console.log(n));
+
+console.log(add(1,2));
+console.log(add(1,2));
+console.log(double(10));
+
+console.log(createObj());
