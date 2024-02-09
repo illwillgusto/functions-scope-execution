211d9a5d85ae1f266c30ed277755be5171ad8a81
Will Walton <will@endless3dge.com>
2024-02-09 15:29:16 -0600

    execution context
    understanding of the execution context and the order of memory before executing code
    
    also refreshed understanding of the browser debugger, added breakpoint pause to then execute lines of code 1 at a time

---
+++ 10-execution-context/script.js
@@ -0,0 +1,12 @@
+const a = 150;
+const b = 100;
+
+function getSum(n1, n2) {
+    const sum = n1 + n2;
+    return sum;
+}
+
+const sum1 = getSum(a, b);
+const sum2 = getSum(10,5);
+
+console.log(sum1, sum2);
